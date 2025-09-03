9c1a92cbe35ab1978ac2455962b6400a0169b3d9
Horoyoi-san <korrawitcompara@gmail.com>
2025-09-03 21:25:50 +0700

    Add files for SR-proxy

--- Guardian/run.bat
+++ Guardian/run.bat
@@ -1,2 +1,2 @@
 @echo off
-start dotnet publish Guardian.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -o ..\publish\tool
+start dotnet publish Guardian.csproj -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:PublishTrimmed=true -o ..\publish\tool
