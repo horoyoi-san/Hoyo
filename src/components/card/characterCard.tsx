2db87e6c321a8d4a248a05f691f2be15c53b96aa
Horoyoi-san <beta.taster.yoi@gmail.com>
2025-09-09 20:52:15 +0700

    Add files for SR-tools

--- src/components/card/characterCard.tsx
+++ src/components/card/characterCard.tsx
@@ -14,55 +14,47 @@ export default function CharacterCard({ data }: CharacterCardProps) {
   const { locale } = useLocaleStore();
   const text = getNameChar(locale, data)
   return (
-    <li 
-    className="z-10 flex flex-col items-center rounded-xl shadow-xl 
-               bg-gradient-to-br from-base-300 via-base-100 to-warning/70
-               transform transition-transform duration-300 ease-in-out 
-               hover:scale-105 cursor-pointer min-h-[110px] sm:min-h-[120px] md:min-h-[150px] lg:min-h-[160px] xl:min-h-[180px] 2xl:min-h-[280px]"
-    >
-      <div
-        className={`w-full rounded-md bg-gradient-to-br ${data.rank === "CombatPowerAvatarRarityType5"
-          ? "from-yellow-400 via-yellow-600/70 to-yellow-800/50"
-          : "from-purple-400 via-purple-600/70 to-purple-800/50"
-          }`}
-      >
-
-        <div className="relative w-full h-full">
-          <Image
-            width={376}
-            height={512}
-            src={`https://api.hakush.in/hsr/UI/avatarshopicon/${data.id}.webp`}
-            priority={true}
-            className="w-full h-full rounded-md object-cover"
-            alt="ALT"
-          />
-          <Image
-            width={32}
-            height={32}
-      
-            src={`/icon/${data.damageType.toLowerCase()}.webp`}
-            className="absolute top-0 left-0 w-6 h-6 rounded-full"
-            alt={data.damageType.toLowerCase()}
-          />
-          <Image
-            width={32}
-            height={32}
-            src={`/icon/${data.baseType.toLowerCase()}.webp`}
-            className="absolute top-0 right-0 w-6 h-6 rounded-full"
-            alt={data.baseType.toLowerCase()}
-            style={{
-              boxShadow: "inset 0 0 8px 4px #9CA3AF"
-            }}
-          />
-        </div>
-      </div>
-
-      <ParseText
-        locale={locale}
-        text={text}
-        className="mt-2 px-1 text-center text-shadow-white font-bold leading-tight 2xl:text-lg"
+<li 
+  className="z-10 flex flex-col items-center rounded-lg shadow-md
+             bg-gradient-to-br from-base-300 via-base-100 to-warning/70
+             transform transition-transform duration-300 ease-in-out 
+             hover:scale-105 cursor-pointer min-h-[90px] sm:min-h-[100px] md:min-h-[120px] lg:min-h-[130px] xl:min-h-[140px] 2xl:min-h-[180px] w-full"
+>
+  <div
+    className={`w-full rounded-md bg-gradient-to-br ${data.rank === "CombatPowerAvatarRarityType5"
+      ? "from-yellow-400 via-yellow-600/60 to-yellow-800/40"
+      : "from-purple-400 via-purple-600/60 to-purple-800/40"
+      }`}
+  >
+    <div className="relative w-full h-32 sm:h-36 md:h-40 lg:h-44 xl:h-48 2xl:h-56">
+      <Image
+        width={150}
+        height={200}
+        src={`https://api.hakush.in/hsr/UI/avatarshopicon/${data.id}.webp`}
+        priority={true}
+        className="w-full h-full rounded-md object-cover"
+        alt="ALT"
       />
-    </li>
+      <Image
+        width={24}
+        height={24}
+        src={`/icon/${data.damageType.toLowerCase()}.webp`}
+        className="absolute top-1 left-1 w-6 h-6 rounded-full"
+        alt={data.damageType.toLowerCase()}
+      />
+      <Image
+        width={24}
+        height={24}
+        src={`/icon/${data.baseType.toLowerCase()}.webp`}
+        className="absolute top-1 right-1 w-6 h-6 rounded-full"
+        style={{ boxShadow: "inset 0 0 6px 3px #9CA3AF" }}
+        alt={data.baseType.toLowerCase()}
+      />
+    </div>
+  </div>
+
+</li>
+
 
   );
 }
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,10 @@
 
 export default function Footer() {
     return(
-        <footer className="footer footer-horizontal footer-center bg-base-200 text-base-content rounded p-10">
+        <footer className="footer footer-horizontal footer-center bg-base-100 text-base-content rounded p-0">
 
             <aside>
-                <p>Copyright © {new Date().getFullYear()} - Kain (Powered by Nextjs & DaisyUi)</p>
+                <p>Copyright © {new Date().getFullYear()} - Horoyoi-san (Powered by Nextjs & DaisyUi)</p>
             </aside>
         </footer>
     )
