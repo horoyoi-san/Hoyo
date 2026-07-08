4e045b030e76179a82d92b0a5ae46c7a6a3cf7d1
Horoyoi-san <korrawitcompara@gmail.com>
2026-07-08 21:27:00 +0700

    update hkrpg

--- Timeline/Timeline Honkai Star Rail 3.8 42+21.py
+++ Timeline/Timeline Honkai Star Rail 3.8 42+21.py
@@ -4,22 +4,28 @@ from timeline_utils import bangkok_datetime, run_timeline
 CONFIG = {
     "game_name": "Honkai Star Rail",
     "start_dates": {
-        "Drip": bangkok_datetime(2025, 12, 30, 11, 0),
-        "Beta": bangkok_datetime(2026, 1, 6, 10, 0),
-        "Release": bangkok_datetime(2026, 2, 18, 10, 0),
+        "Drip": bangkok_datetime(2025, 4, 6, 11, 0),
+        "Beta": bangkok_datetime(2026, 4, 21, 10, 0),
+        "Live": bangkok_datetime(2026, 5, 22, 10, 0),
+        "Predownload": bangkok_datetime(2026, 6, 1, 13, 0),
+        "Release": bangkok_datetime(2026, 6, 3, 10, 0),
     },
     "intervals": {
         "Drip": 42,
         "Beta": 42,
+        "Live": 42,
+        "Predownload": 42,
         "Release": 42,
     },
     "phase_labels": {
         "Drip": "Drip",
         "Beta": "Beta",
+        "Live": "Live",
+        "Predownload": "Predownload",
         "Release": "Release",
     },
-    "start_version": 4.0,
-    "end_version": 15.0,
+    "start_version": 4.3,
+    "end_version": 8.0,
     "output_file": "data/hkrpg_42+21.txt",
     "bullet": True,
 }
