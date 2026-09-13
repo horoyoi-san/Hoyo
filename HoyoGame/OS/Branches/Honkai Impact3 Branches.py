2319400ee61198b68085e8bddf47202db06d9a09
Horoyoi-san <korrawitcompara@gmail.com>
2026-09-13 16:25:25 +0700

    Update Honkai Impact3 Branches.py

--- HoyoGame/OS/Branches/Honkai Impact3 Branches.py
+++ HoyoGame/OS/Branches/Honkai Impact3 Branches.py
@@ -352,7 +352,7 @@ async def main():
         g["id"]: {
             "name": g["display"]["name"],
             "icon": g["display"]["icon"]["url"],
-            "bg": g["display"]["background"]["url"],
+            "bg": (g.get("display", {}).get("background") or {}).get("url"),
         }
         for g in resp["data"]["games"]
     }
